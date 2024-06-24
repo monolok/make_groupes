4f5bbedae8ec932bf1fa190486ef5fe7b4730f26
monolok <antoinebertin@me.com>
2024-06-24 12:19:35 +0200

    docker

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 from functions import create_groups
 import streamlit as st
 import pandas as pd
-
+#from dotenv import load_dotenv
+import os
+#load_dotenv()
+pass_answer=os.environ["PASS"]
 # Initialize or get the current state of 'button_clicked'
 if 'button_clicked' not in st.session_state:
     st.session_state['button_clicked'] = False
 
-group_size = 3
+# Simple password input
+password = st.text_input("Enter the password", type="password")
 
-# Button to trigger the create_groups function
-if st.button('Create Groups'):
-    st.session_state['button_clicked'] = True  # Update the state to indicate the button has been clicked
-    groups = create_groups(group_size)
-    st.write('Groups created:')
-    df = pd.read_csv('data/file.csv')
-    st.dataframe(df)
+group_size = 3
+# Check if the correct password is entered
+if password == pass_answer:
+    # Button to trigger the create_groups function
+    if st.button('Create Groups'):
+        st.session_state['button_clicked'] = True  # Update the state to indicate the button has been clicked
+        groups = create_groups(group_size)
+        st.write('Groups created:')
+        df = pd.read_csv('data/file.csv')
+        st.dataframe(df)
+    else:
+        st.write('Please enter the correct password')
 
 # Display the dataframe only if the button has not been clicked
 if not st.session_state['button_clicked']:
